3536e9aecfc966f216ec57b01d0bdafc5d1c3526
Kristina Bulatović <bulatovic.tina95@gmail.com>
2018-04-03 13:33:49 +0200

    Add files via upload

---
+++ get_set/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace get_set
+{
+    public partial class Form2 : Form
+    {
+        public string a { get; set; }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //this.a = textBox1.Text;
+            //this.DialogResult = DialogResult.OK;
+            //this.Close();
+
+            a = textBox1.Text;
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
